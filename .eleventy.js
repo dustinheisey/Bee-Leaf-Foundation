e819c82fd179199ef202c236c134d7adfdf54302
Dustin Heisey <dustin@heisey.email>
2023-09-10 17:25:17 -0500

    Revert "tweak"
    This reverts commit ce778f6bfa4da41678fdff4cfacec5e0761194d2.

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,6 @@ const common = require('@dh253/nds')
 
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPlugin(common, {
-    domain: 'https://urban-visionaries.dustinheisey.com',
+    domain: 'https://beeleaf.dustinheisey.com',
   })
 }
